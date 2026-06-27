00fbc52fe442b9d2735d8e420145c2f6a8d9428e
sanjeet <sanjeet215@gmail.com>
2026-06-27 20:55:35 +0530

    Initial commit

---
+++ src/main/java/com/email/declutter_ai/DeclutterAiApplication.java
@@ -0,0 +1,13 @@
+package com.email.declutter_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclutterAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclutterAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email/declutter_ai/DeclutterAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.declutter_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclutterAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
